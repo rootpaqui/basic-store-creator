ac633cfe5473f78474f130d9a0edd12563e542e5
PAQUI <paquiromeo6@gmail.com>
2025-02-02 19:42:05 +0100

    Refactor login and layout views for improved styling and accessibility

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,8 +16,8 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
+            <div class="bg-gray-800 py-3 w-full sm:max-w-md flex justify-center">
+                <a href="/" class="inline-block uppercase text-white">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
