dc846fef70095c0bff5f1786797a8a9fbed3b3c3
PAQUI <paquiromeo6@gmail.com>
2025-02-02 19:41:20 +0100

    Add StoreController, StoreRequest, and Store model for store management

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRequest;
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(string $storeName)
+    {
+        $store = Store::where('name', $storeName)->firstOrFail();
+
+        return view('store-front', ['store' => $store]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $store = Store::create($request->validated());
+
+        $storeDomain = str_replace('://', '://'.$store->name.'.', config('app.url'));
+
+        return redirect($storeDomain);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Store $store)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Store $store)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Store $store)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Store $store)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'user_id' => $this->user()->id,
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique:stores,name'],
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+        ];
+    }
+}
